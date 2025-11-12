cb77f1ca324fa7718b5b0812c67a3d9e7e0d1761
Jeff Lab <jdotcafe@gmail.com>
2025-11-12 09:36:07 -0500

    Add example for dataloading.

--- scripts/run_from_hf_datasets.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-
-# Login using e.g. `huggingface-cli login` to access this dataset
-ds = load_dataset("jeffbbrown2/ConnectomeBench", "MICrONS, Segment Classification", split="train")
-
-ds = load_dataset("jeffbbrown2/ConnectomeBench", "MICrONS, Split Error Correction", split="train")
-
-ds = load_dataset("jeffbbrown2/ConnectomeBench", "MICrONS, Merge Error Identification", split="train")
